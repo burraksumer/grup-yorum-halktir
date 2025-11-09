e5377423159ad7dd6c12f9abbb225156a9c6933c
burraksumer <14909074+burraksumer@users.noreply.github.com>
2025-11-10 00:21:27 +0300

    Add seeding option

--- lib/grup_yorum_halktir_phoenix/release.ex
+++ lib/grup_yorum_halktir_phoenix/release.ex
@@ -18,6 +18,14 @@ defmodule GrupYorumHalktirPhoenix.Release do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
+  def seed do
+    load_app()
+
+    # Execute the seeds.exs file
+    seeds_path = Path.join([Application.app_dir(:grup_yorum_halktir_phoenix), "priv/repo/seeds.exs"])
+    Code.eval_file(seeds_path)
+  end
+
   defp repos do
     Application.fetch_env!(@app, :ecto_repos)
   end
